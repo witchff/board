a61cd380eb66822ec5a6bb5ebfd8701dfa6c76dd
Sergei <witchff@mail.ru>
2023-12-20 22:20:54 +0300

    create input component, but need improve

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import Button from 'shared/UI/Button/Button';
 import styles from './App.module.scss';
+import InputContainer, {
+  Input,
+  InputText,
+} from 'shared/UI/Input/Input';
 
 function App() {
   return (
     <div className={styles.app}>
-      <Button
-        as={'button'}
-        size='medium'
-        variant='fill'
-        backColor='gray'
-      >
-        Оформить доставку
-      </Button>
+      <InputContainer id='name' width='420px'>
+        <InputText
+          style={{
+            color: '#828b8d',
+            fontSize: '12px',
+            fontWeight: '400',
+            lineHeight: '20px',
+            letterSpacing: '-0.24px',
+            marginBottom: '10px',
+          }}
+        >
+          Ваш email
+        </InputText>
+        <Input />
+        <InputText
+          style={{
+            color: '#eb5757',
+            fontSize: '12px',
+            fontWeight: '500',
+            lineHeight: '25px',
+            letterSpacing: '-0.24px',
+            marginBottom: '4px',
+            textAlign: 'right',
+          }}
+        >
+          Ваш email
+        </InputText>
+      </InputContainer>
     </div>
   );
 }
